221bae51233a195d2a2231079505167279ba8a1f
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-08-03 04:02:03 -0500

    chore: configure trpc

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import * as trpc from "@trpc/server";
+import * as trpcNext from "@trpc/server/adapters/next";
+
+export const appRouter = trpc.router().query("hello", {
+  input: z
+    .object({
+      text: z.string().nullish(),
+    })
+    .nullish(),
+  resolve({ input }) {
+    return {
+      greeting: `hello ${input?.text ?? "world"}`,
+    };
+  },
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+// export API handler
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext: () => null,
+});
