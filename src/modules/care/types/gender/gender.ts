eddf8bb00babfc4c42fb5e1026b4fa070c67dcd2
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-08-03 04:54:18 -0500

    feat: add gender type

---
+++ src/modules/care/types/gender/gender.ts
@@ -0,0 +1,20 @@
+import * as z from "zod";
+
+export const GENDERS = ["Male", "Female", "Other"] as const;
+
+export const schema = z.enum(GENDERS);
+
+export type Type = z.infer<typeof schema>;
+
+export const validate = (value: unknown): Type => {
+  return schema.parse(value);
+};
+
+export const isValid = (value: unknown): value is Type => {
+  try {
+    validate(value);
+    return true;
+  } catch {
+    return false;
+  }
+};
