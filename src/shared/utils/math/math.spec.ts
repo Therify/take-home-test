9a4f827bffe7b9730d0c6bec126ae6484be10a38
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-08-03 03:21:12 -0500

    test: validate lint-staged

--- src/shared/utils/math/math.spec.ts
+++ src/shared/utils/math/math.spec.ts
@@ -1,9 +1,15 @@
-import { add } from "./math";
+import { add, subtract } from "./math";
 
 describe("math", function () {
   describe("addition", function () {
     it("should add two numbers", function () {
       expect(add(1, 2)).toBe(3);
+    });
+  });
+
+  describe("subtraction", function () {
+    it("should subtract two numbers", function () {
+      expect(subtract(1, 1)).toBe(0);
     });
   });
 });
