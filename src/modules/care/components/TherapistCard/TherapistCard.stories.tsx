1b408d6f0b4e6331fcd997e80e768ee435cd7114
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-08-08 08:39:26 -0500

    chore: remove unused styles

---
+++ src/modules/care/components/TherapistCard/TherapistCard.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { TherapistCard } from "./TherapistCard";
+
+const meta: Meta<typeof TherapistCard> = {
+  title: "Modules/Care/Components/TherapistCard",
+  component: TherapistCard,
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof TherapistCard> = {};
