2b2df5491e936a5bbc1d7e697a3ce9b568963b20
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2023-08-15 18:04:30 -0500

    chore: format tests

--- src/shared/utils/member-preferences/memberPreferences.spec.ts
+++ src/shared/utils/member-preferences/memberPreferences.spec.ts
@@ -3,26 +3,27 @@ import { Provider } from "@/modules/care/types/provider";
 import { providersFilteredByPreferences } from "./memberPreferences";
 
 const defaultPreferences: MemberPreferences.Type = {
-    gender: "Any",
-    designation: "Any",
-    specialty: "Any",
-    state: "Any",
-    insurance: "Any",
-    ethnicity: "Any",
-}
+  gender: "Any",
+  designation: "Any",
+  specialty: "Any",
+  state: "Any",
+  insurance: "Any",
+  ethnicity: "Any",
+};
 
 describe("memberPreferences", function () {
-    describe("filter providers with null preferences", function () {
-        it("should return the providers that were passed in", function () {
-            const providers = []
-            for (const provider of Array.from({ length: 25 }).map(() =>
-                Provider.generateFakeProvider() as Provider.WithPersistedProps
-            )) {
-                providers.push(provider)
-            }
-            
-            expect(providersFilteredByPreferences(providers, defaultPreferences)).toStrictEqual(providers);
-        });
-    });
-});
+  describe("filter providers with null preferences", function () {
+    it("should return the providers that were passed in", function () {
+      const providers = [];
+      for (const provider of Array.from({ length: 25 }).map(
+        () => Provider.generateFakeProvider() as Provider.WithPersistedProps
+      )) {
+        providers.push(provider);
+      }
 
+      expect(
+        providersFilteredByPreferences(providers, defaultPreferences)
+      ).toStrictEqual(providers);
+    });
+  });
+});
