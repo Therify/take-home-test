bd50d86e41d9e61354d57f0cc792b7bde5047c2d
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2023-08-15 17:54:13 -0500

    test: add first test for provider filter

---
+++ src/shared/utils/member-preferences/memberPreferences.spec.ts
@@ -0,0 +1,28 @@
+import { MemberPreferences } from "@/modules/care/types/member-preferences";
+import { Provider } from "@/modules/care/types/provider";
+import { providersFilteredByPreferences } from "./memberPreferences";
+
+const defaultPreferences: MemberPreferences.Type = {
+    gender: "Any",
+    designation: "Any",
+    specialty: "Any",
+    state: "Any",
+    insurance: "Any",
+    ethnicity: "Any",
+}
+
+describe("memberPreferences", function () {
+    describe("filter providers with null preferences", function () {
+        it("should return the providers that were passed in", function () {
+            const providers = []
+            for (const provider of Array.from({ length: 25 }).map(() =>
+                Provider.generateFakeProvider() as Provider.WithPersistedProps
+            )) {
+                providers.push(provider)
+            }
+            
+            expect(providersFilteredByPreferences(providers, defaultPreferences)).toStrictEqual(providers);
+        });
+    });
+});
+
